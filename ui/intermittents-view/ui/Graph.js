540ae5edf6ce2ec9a9e1010c78804e305528669f
Sarah Clements <sclements313@gmail.com>
2018-01-15 16:37:04 -0800

    first graph working with test data

--- ui/intermittents-view/ui/Graph.js
+++ ui/intermittents-view/ui/Graph.js
@@ -2,38 +2,6 @@ import React from 'react';
 import MG from 'metrics-graphics';
 import 'metrics-graphics/dist/metricsgraphics.css';
 // import * as d3 from 'd3';
-// import { connect } from 'react-redux';
-// import { fetchBugData } from './../redux/actions';
-// import PropTypes from 'prop-types';
-// import { apiUrlFormatter } from '../constants';
-
-
-const data = [
-    {
-        date: "2014-01-01",
-        value: 190000000
-    },
-    {
-        date: "2014-01-02",
-        value: 190379978
-    },
-    {
-        date: "2014-01-03",
-        value: 90493749
-    },
-    {
-        date: "2014-01-04",
-        value: 190785250
-    },
-    {
-        date: "2014-01-05",
-        value: 190000000
-    },
-    {
-        date: "2014-01-06",
-        value: 90493749
-    }
-];
 
 export default class Graph extends React.Component {
   constructor(props) {
@@ -44,24 +12,18 @@ export default class Graph extends React.Component {
     this.updateTarget = this.updateTarget.bind(this);
   }
 
-componentDidMount() {
-    // let url = apiUrlFormatter('count', this.props.ISOfrom, this.props.ISOto, this.props.tree);
-    // this.props.fetchData(url, 'BUGS');
-}
-
 shouldComponentUpdate(nextProps, nextState) {
     return nextState.target !== this.state.target;
 }
 
 componentDidUpdate() {
-    // let url = apiUrlFormatter('count', this.props.ISOfrom, this.props.ISOto, this.props.tree);
-    // this.props.fetchData(url, 'BUGS');
-    this.createGraphic(data);
+    this.createGraphic(this.props.data);
 }
 
 createGraphic(data) {
     data = MG.convert.date(data, "date");
     MG.data_graphic({
+        title: "Orange Count Per Push",
         data: data,
         width: 700,
         height: 300,
@@ -80,8 +42,6 @@ updateTarget(element) {
 }
 
 render() {
-    // console.log(moment("1970-01-01").format("x")); <-- unix timestamp conversion
-
     return (
         <div className="mx-auto pb-4" ref={ ele => this.updateTarget(ele) }></div>
         );
--- ui/intermittents-view/ui/IntermittentsView.js
+++ ui/intermittents-view/ui/IntermittentsView.js
@@ -10,20 +10,77 @@ import BugColumn from './BugColumn';
 import { apiUrlFormatter } from '../constants';
 import Graph from './Graph';
 
+const oranges = {
+    "2017-12-28": {
+       orangecount: 157,
+       testruns: 5,
+    },
+    "2017-12-27": {
+       orangecount: 142,
+       testruns: 5,
+    },
+    "2017-12-26": {
+       orangecount: 89,
+       testruns: 4,
+    },
+    "2017-12-25": {
+       orangecount: 58,
+       testruns: 2,
+    },
+    "2017-12-24": {
+       orangecount: 193,
+       testruns: 6,
+    },
+    "2017-12-23": {
+       orangecount: 191,
+       testruns: 7,
+    },
+    "2017-12-22": {
+       orangecount: 132,
+       testruns: 5,
+    },
+    "2017-12-21": {
+       orangecount: 222,
+       testruns: 6,
+    }
+ };
+
 export class IntermittentsView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+        dateCounts: null,
+        dateTestRunCounts: null,
+        dateFreqs: null
     };
   }
 
 componentDidMount() {
     let url = apiUrlFormatter('bugs', this.props.ISOfrom, this.props.ISOto, this.props.tree);
     this.props.fetchData(url, 'BUGS');
+    this.parseGraphData(oranges);
+}
+
+parseGraphData(data) {
+    let dateCounts = [];
+    let dateTestRunCounts = [];
+    let dateFreqs = [];
+    Object.entries(data).map((entry) => {
+        let freqs = entry[1].testruns < 1 ? 0 : (entry[1].orangecount/entry[1].testruns);
+        [dateCounts, dateTestRunCounts, dateFreqs] = this.calculateMetric(entry[0], entry[1].orangecount,
+            entry[1].testruns, freqs, dateCounts, dateTestRunCounts, dateFreqs);
+    });
+    this.setState({ dateCounts, dateTestRunCounts, dateFreqs });
+}
+
+calculateMetric(date, count, testruns, freq, dateCounts, dateTestRunCounts, dateFreqs) {
+    dateCounts.push({ date, value: count });
+    dateTestRunCounts.push({ date, value: testruns });
+    dateFreqs.push({ date, value: freq.toFixed(2) });
+    return [dateCounts, dateTestRunCounts, dateFreqs];
 }
 
 render() {
-    // console.log(moment('1970-01-01').format('x')); <-- unix timestamp conversion
     const { bugs, failureMessage, from, to, ISOfrom, ISOto } = this.props;
     const columns = [
         {
@@ -44,7 +101,7 @@ render() {
           accessor: 'whiteboard',
         }
       ];
-
+    console.log(this.state.dateCounts, this.state.dateTestRunCounts);
     return (
         <Container fluid style={{ marginBottom: '.5rem', marginTop: '5rem', maxWidth: '1200px' }}>
             <Navigation name='BUGS' ISOfrom={ISOfrom} ISOto={ISOto} endpoint='bugs'/>
@@ -55,10 +112,11 @@ render() {
                 <Col xs='12' className='mx-auto'><p className='subheader'>{`${from} to ${to}`}</p></Col>
             </Row>
             <Row>
-                <Col xs='12' className='mx-auto'><p className='text-secondary'>X failures in X pushes</p></Col>
+                <Col xs='12' className='mx-auto pb-5'><p className='text-secondary'>X failures in X pushes</p></Col>
             </Row>
             <Row>
-                <Graph />
+                {this.state.dateFreqs &&
+                <Graph data={this.state.dateFreqs} />}
             </Row>
             {/* TODO: Set up manual/server side table sorting/pagination with redux*/}
             <DateRangePicker name='BUGS'/>
